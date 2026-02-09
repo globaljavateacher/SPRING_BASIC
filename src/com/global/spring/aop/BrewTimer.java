a1270a280a9b99041a851daf45ad013666915d01
GUM MINJAE (琴旼賳) <globaljavateacher@gmail.com>
2026-02-09 17:06:31 +0900

    Update BrewTimer.java

--- src/com/global/spring/aop/BrewTimer.java
+++ src/com/global/spring/aop/BrewTimer.java
@@ -4,14 +4,11 @@ import org.aspectj.lang.ProceedingJoinPoint;
 
 public class BrewTimer {
 	public Object  check(ProceedingJoinPoint joinPoint) throws Throwable {
-		long s = System.nanoTime();
-		
-		Object result = joinPoint.proceed();
-		
+		long s = System.nanoTime();		
+		Object result = joinPoint.proceed();		
 		long e = System.nanoTime();
-		System.out.printf("Brew Time: %d ms", (e - s) / 1_000_000);
-		
-		return result;
-		
+		System.out.printf("Brew Time: %d ms", (e - s) / 1_000_000);		
+		return result;		
 	}
 }
+
